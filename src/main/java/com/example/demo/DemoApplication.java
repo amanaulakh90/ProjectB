35be68bbf2e14a3de4b4030bc25f436eda2f78a3
amanaulakh90 <81429712+amanaulakh90@users.noreply.github.com>
2021-10-08 22:44:59 -0600

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,7 @@ public class DemoApplication {
 
 	@GetMapping("/")
 	String home() {
-		return "Spring is here!";
+		return "Spring is here! - Testing";
 	}
 
 	public static void main(String[] args) {
